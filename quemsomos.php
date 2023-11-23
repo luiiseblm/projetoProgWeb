6349481b129c7201719b3478a6d78195f7b03c6a
lukasreei <130949016+lukasreei@users.noreply.github.com>
2023-11-23 13:02:20 -0300

    Add files via upload

---
+++ quemsomos.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+body{ text-align: center;
+    text-align: center;
+    background-image: url("https://i.pinimg.com/originals/0c/0e/2e/0c0e2ec0e55b0f3b6244c749f06ce6bb.png");
+    background-repeat: no-repeat;
+    background-position: right top;
+
+    }
+    h2{ text-align: left;
+
+    }
+</style>    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+    <h1>QUEM SOMOS?</h1>
+    <br><br>
+    <BR><br>
+<h2>
+    somos um escritorio de advogados, que possui o intuito de ajudar outros advogados e empresarios<br>
+    no dia a dia com suas burocracias, que limitam seu trabalho diario e reduz o rendimento.
+    <p>
+        estamos localizados no BAIRRO DOS YPES, RUA JARDIM VARGAS, NÚMERO 510</p>
+        traga seus documentos e problemas, para que possamos ajudar a resolver e tirar o fardo de suas costas.
+
+    <p>
+        entre em contato pelo TELL: 3210-0123
+    
+</h2>
+</body>
+</html>
